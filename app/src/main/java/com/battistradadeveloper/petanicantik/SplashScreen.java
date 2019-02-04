6c2d1985b7597b4f32c591dc64ba64f937c7789c
aridwiprayogo <kisamberbledek@yahoo.com>
2019-02-05 05:41:51 +0700

    fix splash screen

--- app/src/main/java/com/battistradadeveloper/petanicantik/SplashScreen.java
+++ app/src/main/java/com/battistradadeveloper/petanicantik/SplashScreen.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
     private int time_load = 4000;
+    ImageView imageSplash;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        imageSplash = findViewById(R.id.logo);
 
         new Handler().postDelayed(new Runnable() {
             @Override
